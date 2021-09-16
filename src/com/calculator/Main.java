b2bfb3b92e2405021a1db98c39123975950f6f4b
LAPTOP-UEEQEUCE\Пользователь <agoronik@yandex.ru>
2021-09-16 17:53:34 +0300

    Work in arab

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -7,12 +7,22 @@ import static java.lang.Integer.parseInt;
 public class Main {
 
     static String defLang = "";
-    static String [] ARA = {"1","2","3","4","5","6","7","8","9","0"};
-    static String [] ROM = {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
-    static String[] OPR = new String[15];
-    static String[] Numbers = new String[15];
+    static String [] ARA = {"0","1","2","3","4","5","6","7","8","9","10"};
+    static String [] ROM = {"", "I","II","III","IV","V","VI","VII","VIII","IX","X"};
+    static String[] OPR = new String[5];
+    static String[] Numbers = new String[5];
     static int error = 0;
-    static String [] errorText = {"Нет ошибок","Не правильный воод чисел","","","","","",};
+
+    /* Сообщения об ошибках */
+    static String [] errorText = {
+    /*0*/       "Нет ошибок",
+    /*1*/       "Не правильный ввод чисел",
+    /*2*/       "Не правильный ввод чисел, допустимые значения для ввода от 1 до 10 или I II III ... X",
+    /*3*/       "",
+    /*4*/       "",
+    /*5*/       "",
+    /*6*/       ""
+            };
 
     public static void main(String [] args) {
 
@@ -21,8 +31,7 @@ public class Main {
         System.out.print("Введите выражение: ");
         String str = in.next();
 
-        //System.out.print(str);
-
+        // проверка ввода на знаки(+ - * /) и значения(цифры) собираем в массив
         Main.checkOperation(str);
 
         var c = Arrays.deepToString(OPR);
@@ -31,8 +40,10 @@ public class Main {
         var n = Arrays.deepToString(Numbers);
         System.out.print(n);
 
-
-
+        if(OPR.length>=Numbers.length) {
+            error = 2;
+            ShowError();
+        }
 
         if(!defLang.equals("err")) {
 
@@ -42,16 +53,25 @@ public class Main {
             Main.calc();
 
         } else {
-            System.out.println();
-            System.out.print(errorText[error]);
+            ShowError();
         }
 
         in.close();
     }
 
-    static String checkOperation(String str) {
+    static void ShowError() {
+        System.out.println();
+        System.out.print(errorText[error]);
+    }
+
+    //static boolean check_naumbers([] )
+
+    static void checkOperation(String str) {
         String[] operators = {"-","+","/","*"};
         String curNumber = "";
+
+        str = str.trim();
+
         var index = str.length() - 1;
         var i = 0;
 
@@ -60,31 +80,44 @@ public class Main {
         while (i <= index) {
             var j = 0;
             var oper = Character.toString( str.charAt(i));
-            //System.out.print(oper);
 
-            while (j <= operators.length - 1) {
-                var operToEq = operators[j];
-
-                if(oper.equals(operToEq) ) {
-                    OPR[o] = oper;
-                    Numbers[o] = curNumber;
-                    curNumber = "";
-                    o++;
-                }
-
-                j++;
-            }
+            System.out.println("------------");
+            System.out.println(oper);
+            System.out.println(oper.length());
+            System.out.println("------------");
 
             var v = Main.checkIsVal(oper);
 
             if (v >= 0) {
-                curNumber += v;
+
+                if(defLang.equals("arab")) {
+                    curNumber += v;
+                }
+
+                if(defLang.equals("rome")) {
+                    curNumber += oper;
+                }
 
                 if(i == index) {
                     Numbers[o] = curNumber;
                 }
+
+
             } else {
-                System.out.println("Error charcters wrong");
+
+                while (j <= operators.length - 1) {
+                    var operToEq = operators[j];
+
+                    if(oper.equals(operToEq) ) {
+                        OPR[o] = oper;
+                        Numbers[o] = curNumber;
+                        curNumber = "";
+                        o++;
+                    }
+
+                    j++;
+                }
+                //System.out.println("Error charcters wrong");
                 //errr
             }
 
@@ -96,15 +129,19 @@ public class Main {
         }
 
 
-        return "sdsd";
     }
 
     static int checkIsVal(String a) {
-        var i = 0;
         var resStr = "";
         var is_rome = 0;
         var is_arab = 0;
+        var i = 1;
+        var romeIndex = 0;
+
+        System.out.println("oper " + a);
+
         while(i <= ROM.length - 1) {
+
             if(a.equals(ROM[i])) {
                 is_rome = 1;
                 if(defLang.equals("arab")) {
@@ -115,6 +152,7 @@ public class Main {
                 }
 
                 resStr = ROM[i];
+                romeIndex = i;
             }
             i++;
         }
@@ -140,16 +178,49 @@ public class Main {
         }
 
         if(error == 0) {
-           var res = parseInt(a);
-            return res;
+
+            if(is_arab > 0) {
+                return parseInt(a);
+            }
+            if(is_rome > 0) {
+                return romeIndex;
+            }
+
         } else {
             return -1;
         }
 
-
+        return 0;
     }
 
     static void calc() {
+        int result = 0;
+
+        if(defLang.equals("arab")) {
+            var i = 0;
+            while (i <= OPR.length - 1) {
+                //if(O)
+
+                if(OPR[i] != null) {
+                    int a = parseInt(Numbers[i]);
+                    int b = parseInt(Numbers[i+1]);
+
+                    if(OPR[i].equals("+")) result = result == 0 ? a + b : result + b;
+                    if(OPR[i].equals("-")) result = result == 0 ? a - b : result - b;
+                    if(OPR[i].equals("/")) result = result == 0 ? a / b : result / b;
+                    if(OPR[i].equals("*")) result = result == 0 ? a * b : result * b;
+
+                }
+                i++;
+            }
+
+            System.out.println("Результат: " + result);
+        }
+
+        if(defLang.equals("rome")) {
+
+        }
+
         var i = 0;
         while (i <= OPR.length - 1) {
 
@@ -160,4 +231,6 @@ public class Main {
             i++;
         }
     }
+
+
 }
